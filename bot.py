a4a7099125fe88fb8e10f29ffefe3d3989ed2f05
gabitov shamil <gonjubazz@gmail.com>
2020-08-10 15:23:57 +0500

    Added: script: config.py Updated: bot.py info: telegram bot logic

--- bot.py
+++ bot.py
@@ -1,1 +1,33 @@
-print('hello world')
+import telebot
+
+from pyowm.commons.exceptions import APIRequestError, NotFoundError
+from pyowm.owm import OWM
+from pyowm.config import DEFAULT_CONFIG
+from config import TELEGRAM_TOKEN, PYOWM_TOKEN
+token=TELEGRAM_TOKEN
+
+
+bot=telebot.TeleBot(token)
+owm=OWM(PYOWM_TOKEN)
+
+weather_mgr = owm.weather_manager()
+
+@bot.message_handler(commands=['start', 'help'])
+def send_info(message):
+    bot.send_message(message.chat.id, 'Напиши город в формате(Город Москва) и будет выдана информация о погоде:3')
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text[:6].lower() == 'город ':
+
+        try:
+            print(message)
+            weather = weather_mgr.weather_at_place(message.text[6:]).weather
+            temp=weather.temperature('celsius')
+            bot.send_message(message.chat.id, 'Температура в городе ' + message.text[6:] + ' '+ str(temp['temp']) + str('°'))
+        except APIRequestError:
+            bot.send_message(message.chat.id, 'Город не найден:(')
+        except NotFoundError:
+            bot.send_message(message.chat.id, 'Город не найден:(')
+    else:
+        bot.send_message(message.chat.id, 'Неизвестная команда')
+bot.polling()
